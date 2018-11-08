47331f0a7ae8ab940d32b4404a11341c4d136bca
codeChengeWord <tangxkwork@163.com>
2018-11-08 19:44:39 +0800

    分页完成

--- src/view/index/list.js
+++ src/view/index/list.js
@@ -17,7 +17,7 @@
 * @flow
 */
 import React, { Component } from 'react'
-import {List,Avatar} from 'antd'
+import { List, Avatar} from 'antd'
 import {Link} from 'react-router-dom'
 import TxtTag from '../txtTag'
 import {connect} from 'react-redux'
@@ -25,30 +25,38 @@ import axios from 'axios'
 class IndexList extends Component {
   constructor(arg){
     super(arg);
+    this.isState =true;
     this.state={
       page:1
     }
-    this.getData(this.props.tab)
+    this.getData(this.props.tab,this.state.page)
   }
   /**生命周期函数
    * 当state中的数据更新
    * 判断是否需要重新渲染
    */
-  shouldComponentUpdate(nextProps){
-   // console.log(nextProps)
+  shouldComponentUpdate(nextProps,nextState){
+    this.isState=false;
+    /**分页切换重新渲染 */
+    if(this.state.page!==nextState.page){
+      this.getData(nextProps.tab,nextState.page)
+      return true
+    }
     if(this.props.tab!==nextProps.tab){
-      this.getData(nextProps.tab)
+      this.setState({
+        page:1
+      })
+      this.getData(nextProps.tab,1)
       return false
     }
     return true
   }
-  getData(tab){
-    let {page} = this.state
+  getData(tab,page){
     this.props.dispatch((dispatch)=>{
       dispatch({
         type: "LIST_UPDATA"
       });
-      axios.get(`https://cnodejs.org/api/v1/topics?tab=${tab}&page=${page}&limit=15`)
+      axios.get(`https://cnodejs.org/api/v1/topics?tab=${tab}&page=${page}&limit=10`)
       .then((res)=>{
         /**请求成功回调方法 */
         dispatch({
@@ -65,9 +73,23 @@ class IndexList extends Component {
   }
   render() {
     let {loading,data} = this.props
+    /**分页应用 */
+    let pagination ={
+      current:this.state.page,
+      pageSize:10,
+      total:1000,
+      onChange:((current)=>{
+        this.setState({
+          page:current
+        })
+      })
+    }
+    /**判断是否有数据 */
+    let data_count = data==null?0:data.length
     return <List
           loading={loading}
           dataSource={data}
+          pagination={this.isState || data_count===0?false:pagination}
           renderItem={item=>(<List.Item
             actions={["回复:"+item.reply_count,"访问:"+item.visit_count]}
             key={item.id}>
